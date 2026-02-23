85d06fa2a6ad544f8b93f31ff02715096858beb0
dinraj910 <dinrajkdinesh564@gmail.com>
2026-02-23 14:22:40 +0530

    feat: Add initial Vite configuration for React development with server settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitse.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['8d3f-202-88-229-91.ngrok-free.app'],
+    port: 5173,
+    host: true,
+    strictPort: true,
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [react()],
 })
